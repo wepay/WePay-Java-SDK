0716641e4900b2d69fcb35bd406fc6756711967d
Weina Scott <weina@wepay.com>
2015-08-21 14:46:58 -0700

    remove build_charge_back function that is not used

--- src/main/java/com/wepay/model/data/ChargebackData.java
+++ src/main/java/com/wepay/model/data/ChargebackData.java
@@ -5,11 +5,4 @@ import org.json.*;
 public class ChargebackData {
 	public BigDecimal amountChargedBack;
 	public String disputeUri;
-
-	public static JSONObject build_chargeback(ChargebackData info) throws JSONException {
-		JSONObject o = new JSONObject();
-		o.put("amount_charged_back", info.amountChargedBack);
-		o.put("dispute_uri", info.disputeUri);
-		return o;
-	}
 }
