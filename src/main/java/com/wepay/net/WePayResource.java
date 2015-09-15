3ddbc9500bbca8632c74d7a30963770d9841300e
Biplap Sarkar <biplap.sarkar@wepay.com>
2015-09-15 16:37:35 -0700

    update version

--- src/main/java/com/wepay/net/WePayResource.java
+++ src/main/java/com/wepay/net/WePayResource.java
@@ -48,7 +48,7 @@ public class WePayResource {
 		connection.setRequestMethod("POST");
 		connection.setRequestProperty("Content-Type", "application/json");
 		connection.setRequestProperty("Api-Version", "2015-08-15");
-		connection.setRequestProperty("User-Agent", "WePay Java SDK v3.0.0");
+		connection.setRequestProperty("User-Agent", "WePay Java SDK v3.0.1");
 		if (accessToken != null) {
             connection.setRequestProperty("Authorization", "Bearer " + accessToken);  
         }		
