f2d9654168d7dd19bc6dffaddcbbb89e5f03e97e
Ryan Walker <ryanwalker7@gmail.com>
2016-01-06 13:04:40 -0700

    Add data field to PMCreditCardData so we can send across the transaction_token param that is required for running EMV transactions

--- src/main/java/com/wepay/model/data/PMCreditCardData.java
+++ src/main/java/com/wepay/model/data/PMCreditCardData.java
@@ -1,12 +1,19 @@
 package com.wepay.model.data;
+
 import org.json.*;
+import java.util.Map;
 
 public class PMCreditCardData {
-	public Long id;
-    
-	public static JSONObject buildCreditCard(PMCreditCardData info) throws JSONException {
-		JSONObject o = new JSONObject();
-		o.put("id", info.id);
-		return o;
-	}
+    public Long id;
+
+    public Map<String, Object> data;
+
+    public static JSONObject buildCreditCard(PMCreditCardData info) throws JSONException {
+        JSONObject o = new JSONObject();
+        o.put("id", info.id);
+        if (info.data != null) {
+            o.put("data", info.data);
+        }
+        return o;
+    }
 }
