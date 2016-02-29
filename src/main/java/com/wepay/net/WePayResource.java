c0575e41821b5b566e59716955dd72a80ebe6b3a
Sankate Sharma <sankate@sankate.com>
2016-02-29 10:38:55 -0800

    Update WePayResource.java

--- src/main/java/com/wepay/net/WePayResource.java
+++ src/main/java/com/wepay/net/WePayResource.java
@@ -1,7 +1,6 @@
 package com.wepay.net;
 
 import java.io.BufferedReader;
-import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
