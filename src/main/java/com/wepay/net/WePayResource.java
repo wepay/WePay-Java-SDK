67ce0907dc26f484bf3dbf0d7ac07dc6747d4946
Jacob Kessler <jacobk@wepay.com>
2016-11-11 16:04:44 -0800

    Updated Jar and resource version

--- src/main/java/com/wepay/net/WePayResource.java
+++ src/main/java/com/wepay/net/WePayResource.java
@@ -55,7 +55,7 @@ public class WePayResource {
 		connection.setRequestMethod("POST");
 		connection.setRequestProperty("Content-Type", "application/json");
 		connection.setRequestProperty("Api-Version", "2016-07-13");
-		connection.setRequestProperty("User-Agent", "WePay Java SDK v6.0.0");
+		connection.setRequestProperty("User-Agent", "WePay Java SDK v6.1.0");
 		if (accessToken != null) {
             connection.setRequestProperty("Authorization", "Bearer " + accessToken);  
         }		
