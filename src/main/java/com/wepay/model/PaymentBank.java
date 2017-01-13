44f2d36a620db05bf0605794ce14e01445fd9ace
Cameron Alley <camerona@wepay.com>
2017-01-13 10:27:46 -0800

    Incorporated HeaderData into PaymentBank model

--- src/main/java/com/wepay/model/PaymentBank.java
+++ src/main/java/com/wepay/model/PaymentBank.java
@@ -15,23 +15,35 @@ public class PaymentBank extends WePayResource {
 	protected String state;
 	protected String bankName;
 	protected String accountLastFour;
-	
+
 	public static PaymentBank fetch(Long paymentBankId, String accessToken) throws JSONException, IOException, WePayException {
+		HeaderData headerData = new HeaderData();
+		headerData.accessToken = accessToken;
+		return PaymentBank.fetch(paymentBankId, headerData);
+	}
+
+	public static PaymentBank fetch(Long paymentBankId, HeaderData headerData) throws JSONException, IOException, WePayException {
 		JSONObject params = new JSONObject();
 		params.put("payment_bank_id", paymentBankId);
 		params.put("client_id", WePay.clientId);
 		params.put("client_secret", WePay.clientSecret);
-		String response = request("/payment_bank", params, accessToken);
+		String response = request("/payment_bank", params, headerData);
 		PaymentBank pb = gson.fromJson(response, PaymentBank.class);
 		return pb;
 	}
 
 	public static PaymentBank persist(Long paymentBankId, String accessToken) throws JSONException, IOException, WePayException {
+		HeaderData headerData = new HeaderData();
+		headerData.accessToken = accessToken;
+		return PaymentBank.persist(paymentBankId, headerData);
+	}
+
+	public static PaymentBank persist(Long paymentBankId, HeaderData headerData) throws JSONException, IOException, WePayException {
 		JSONObject params = new JSONObject();
 		params.put("payment_bank_id", paymentBankId);
 		params.put("client_id", WePay.clientId);
 		params.put("client_secret", WePay.clientSecret);
-		String response = request("/payment_bank/persist", params, accessToken);
+		String response = request("/payment_bank/persist", params, headerData);
 		PaymentBank pb = gson.fromJson(response, PaymentBank.class);
 		return pb;
 	}
